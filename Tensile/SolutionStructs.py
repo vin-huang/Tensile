4ced45daa30081f359bcc6f925770309f8709ddd
Vin Huang <vin.huang@amd.com>
2021-09-14 12:25:28 -0500

    MIArchVgpr remove the restriction of the compute data type

--- Tensile/SolutionStructs.py
+++ Tensile/SolutionStructs.py
@@ -3467,12 +3467,6 @@ class Solution(collections.abc.Mapping):
          not state["EnableMatrixInstruction"]:
         reject(state, "MIArchVgpr requires gcn support ACC_CD bit for MatrixInstruction")
         return
-      if not (state["ProblemType"]["ComputeDataType"].isDouble() or state["ProblemType"]["ComputeDataType"].isDoubleComplex() or\
-              state["ProblemType"]["ComputeDataType"].isSingle() or state["ProblemType"]["ComputeDataType"].isSingleComplex() or\
-              (state["ProblemType"]["ComputeDataType"].isHalf() and state["ProblemType"]["HighPrecisionAccumulate"]) or \
-              state["ProblemType"]["ComputeDataType"].isInt32()):
-        reject(state, "MIArchVgpr now only support fp64, fp32, fp16, int8 MatrixInstruction.")
-        return
 
     if state["AtomicAddC"]:
       if not state["ProblemType"]["DataType"].isDouble():
